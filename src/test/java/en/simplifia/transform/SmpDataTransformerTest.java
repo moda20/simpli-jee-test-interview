1bb588b18cb147701aa9458787f9e14c557d619f
moda20 <jaxlegrand@hotmail.fr>
2017-11-23 13:20:16 +0100

    Completed the SmpDataTransformerTest methods

--- src/test/java/en/simplifia/transform/SmpDataTransformerTest.java
+++ src/test/java/en/simplifia/transform/SmpDataTransformerTest.java
@@ -1,8 +1,21 @@
 package en.simplifia.transform;
 
+import fr.simplifia.input.exception.InputException;
 import fr.simplifia.input.validator.SmpInputValidator;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 
 public class SmpDataTransformerTest {
@@ -11,29 +24,53 @@ public class SmpDataTransformerTest {
 
     public SmpDataTransformerTest(){
         validator = Mockito.mock(SmpInputValidator.class);
+        doNothing().when(validator).validateInput(anyString());
         //TODO : mocking strategy
         //when(validator.validateInput(input)).thenReturn();
     }
 
     @Test
     public void testTransformOk() throws Exception {
+        doNothing().when(validator).validateInput("Welcom");
+        try {
+            validator.validateInput("Welcome");
+        }catch (InputException ex){
 
+        }
     }
 
     @Test
     public void testTransformNotOk() throws Exception {
+        doThrow(InputException.class).when(validator).validateInput("é");
+        fail("Invalid Input");
+        try {
+            validator.validateInput("é");
+        }catch (InputException ex){
 
+        }
     }
 
 
     @Test
     public void testTransformEmpty() throws Exception {
+        doThrow(InputException.class).when(validator).validateInput("");
+        fail("Empty Input");
+        try {
+            validator.validateInput("");
+        }catch (InputException ex){
 
+        }
     }
 
     @Test
     public void testTransformNull() throws Exception {
+        doThrow(InputException.class).when(validator).validateInput(null);
+        fail("Null Input");
+        try {
+            validator.validateInput(null);
+        }catch (InputException ex){
 
+        }
     }
 
 
